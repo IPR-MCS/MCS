45b7ed8dee869451c6b711ee19cccf8b9211447b
noway_/ <noe@gabb.fr>
2024-06-04 23:13:37 +0200

    ✨ Customise filepath

--- Working_Porosity_Model.m
+++ Working_Porosity_Model.m
@@ -114,9 +114,11 @@ function generateRandomSimulation(filepath, nbr_cavities, minradius, maxradius)
       'radii','Tcenter','time_to_Curie','tlist');
 end
 
-function generateSimulations(number_of_sim, mincavity, maxcavity, minradius, maxradius) % Generate and save random models under 'Sim{i}.mat where i the indice of the generated.
+function generateSimulations(filepath,number_of_sim, mincavity, maxcavity, minradius, maxradius)
+    % Generate and save random models under a "filepath" which must be configured as "Something{i}.mat" where i is the indice of the simulated object
+    % Giving an example : generateSimulations('Sim%d.mat',1,5,10,200,500)
     for i = 1:1:number_of_sim
-        generateRandomSimulation(sprintf('Sim%d.mat',i),randi([mincavity, maxcavity]), minradius, maxradius);
+        generateRandomSimulation(sprintf(filepath,i),randi([mincavity, maxcavity]), minradius, maxradius);
     end
 end
 
